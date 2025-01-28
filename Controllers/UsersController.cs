1cefdf181c02cf5a1778253546fabe0dfe6e5184
Silviu-Ciprian Saulea <silviu.saulea@connections.tech>
2025-01-28 23:20:19 +0200

    end of section 4

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,15 +1,15 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController(DataContext dataContext) : ControllerBase
+    public class UsersController(DataContext dataContext) : BaseApiController
     {
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -17,6 +17,7 @@ namespace API.Controllers
             return users;
         }
 
+        [Authorize]
         [HttpGet("{id:int}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
@@ -26,25 +27,6 @@ namespace API.Controllers
                 return NotFound();
 
             return user;
-        }
-
-        [HttpPost("SaveUser")]
-        public async Task<ActionResult<AppUser>> SaveUser(string userName)
-        {
-            var user = await dataContext.Users.AddAsync(new AppUser{ UserName = userName });
-            await dataContext.SaveChangesAsync();
-            return user.Entity;
-        }
-
-        [HttpDelete("delete/{id}")]
-        public async Task<ActionResult> DeleteUser(int id)
-        {
-            var user = await dataContext.Users.FindAsync(id);
-            if (user == null)
-                return NotFound();
-            dataContext.Users.Remove(user);
-            await dataContext.SaveChangesAsync();
-            return Ok();
         }
 }
 }
---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(configuration.GetConnectionString("Defaultconnection"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        return services;
+    }
+}
